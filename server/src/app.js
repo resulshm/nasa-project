a337ba8ca27bd9583a419a84ba0fdb634fedbc23
Resul Shamuhammedov <resulshamuhammedov@gmail.com>
2023-10-07 19:04:27 +0500

    feat: ready to deployment, running, serving

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const planetsRouter = require("./router/planets/planets.router");
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
 app.use(planetsRouter);
 
 module.exports = app;
