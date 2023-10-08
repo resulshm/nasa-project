2b274707563e611bad7e1365cd649d3c93902a22
Resul Shamuhammedov <resulshamuhammedov@gmail.com>
2023-10-08 15:23:57 +0500

    refactor: separated models and controllers with data access functions

--- server/src/app.js
+++ server/src/app.js
@@ -18,10 +18,11 @@ app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.json());
 
+app.use(planetsRouter);
+app.use(launchesRouter);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
-
-app.use(planetsRouter);
 
 module.exports = app;
